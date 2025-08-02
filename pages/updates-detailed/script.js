956fa51616c4ade00531c11207e786356f454b4a
Kushagra-Pant <1pantkus@gmail.com>
2025-08-02 12:06:57 -0400

    feat: add page-header class to card elements for consistent styling across multiple pages; implement update history fetching functionality with structured data handling

---
+++ pages/updates-detailed/script.js
@@ -0,0 +1,28 @@
+class Update {
+  constructor(name, date, content){
+    this.name = name
+    this.date = date
+    this.content = content
+  }
+}
+
+fetch('history.txt')
+  .then(response => response.text())
+  .then(text => {
+    const lines = text.split('\n\n')
+    const updates = [];
+    for (block of lines){
+      l = block.split('\n')
+      rawName = l.pop(0)
+      rawDate = l.pop(1)
+      rawContent = "\n".join(l)
+
+      const name = rawName.replace(/[*#]/g, '').trim()
+      const date = rawDate.replace(/\*/g, '').trim()
+      const content = rawContent.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>').trim()
+
+      updates.push(new Update(name, date, content))
+      console.log(content.length)
+    }
+    console.log(updates)
+  });
