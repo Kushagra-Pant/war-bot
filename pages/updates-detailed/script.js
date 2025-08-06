12e8027a97142a774d554caf7b5caf767c2568d6
Kushagra-Pant <1pantkus@gmail.com>
2025-08-06 11:23:11 -0400

    updated updates-overview to add 1.4.3, fixed last-row logic in updates-detailed javascript

--- pages/updates-detailed/script.js
+++ pages/updates-detailed/script.js
@@ -35,7 +35,6 @@ fetch('history.txt')
       }
     }
     let row = [];
-    let widths = {};
 
     for (let i = 0; i < updates.length; i++) {
       let item = updates[i];
@@ -48,55 +47,11 @@ fetch('history.txt')
       if (widthSum + getWidth(item) <= 12) {
         row.push(item);
       } else {
-        let rankedrow = row.slice().sort((a, b) => b.length - a.length);
-        console.log(rankedrow);
-
-        row.forEach(r => {
-          widths[r.name] = getWidth(r);
-        });
-
-        const sumWidths = () => {
-          let sum = 0;
-          for (let key in widths) {
-            sum += widths[key];
-          }
-          return sum;
-        };
-
-        console.log("Widths before:", widths);
-        console.log("Sum before:", sumWidths());
-
-        let num = 0;
-        while (sumWidths() < 12) {
-          let name = rankedrow[num % rankedrow.length].name;
-          widths[name] += 1;
-          num += 1;
-        }
-
-        console.log("Adjusted widths:", widths);
-        console.log("Sum after:", sumWidths());
-
-        let accordionItem = '<div class="row g-4 mb-4">'
-        for(let r = 0; r < row.length; r++){
-          accordionItem += `
-              <div class="col-${widths[row[r].name]}">
-                <div class="card h-100">
-                  <div class="card-body">
-                    <h5 class="card-title text-center">${row[r].name}</h5>
-                    <h6 class="card-subtitle mb-2 text-center">${row[r].date}</h6>
-                    <p class="card-text text-center">${row[r].content}</p>
-                  </div>
-                </div>
-              </div>
-          `
-        }
-        accordionItem += '</div>'
-        container.insertAdjacentHTML('beforeend', accordionItem)
+        renderRow(row, container)
         row = [item]
-        widths = {}
-        console.log('')
-        }
       }
+    }
+    renderRow(row, container)
   });
 
 function getWidth(update){
@@ -121,4 +76,50 @@ function enclose(array, prefix, suffix){
     str += array[array.length - 1];
   }
   return str
+}
+
+function renderRow(row, container){
+  let widths = {}
+  let rankedrow = row.slice().sort((a, b) => b.length - a.length);
+  console.log(rankedrow);
+
+  row.forEach(r => {
+    widths[r.name] = getWidth(r);
+  });
+
+  const sumWidths = () => {
+    let sum = 0;
+    for (let key in widths) {
+      sum += widths[key];
+    }
+    return sum;
+  };
+
+  let num = 0;
+  while (sumWidths() < 12) {
+    let name = rankedrow[num % rankedrow.length].name;
+    widths[name] += 1;
+    num += 1;
+  }
+
+  console.log("Adjusted widths:", widths);
+  console.log("Sum after:", sumWidths());
+
+  let accordionItem = '<div class="row g-4 mb-4">'
+  for(let r = 0; r < row.length; r++){
+    accordionItem += `
+        <div class="col-${widths[row[r].name]}">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title text-center">${row[r].name}</h5>
+              <h6 class="card-subtitle mb-2 text-center">${row[r].date}</h6>
+              <p class="card-text text-center">${row[r].content}</p>
+            </div>
+          </div>
+        </div>
+    `
+  }
+  accordionItem += '</div>'
+  container.insertAdjacentHTML('beforeend', accordionItem)
+  console.log('')
 }
