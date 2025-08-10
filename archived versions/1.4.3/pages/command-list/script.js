2fae89b0cc6845f68fab5ec31b3b45ceeef032b8
Kushagra-Pant <1pantkus@gmail.com>
2025-08-10 12:37:52 -0400

    updated for update 1.4.4

---
+++ archived versions/1.4.3/pages/command-list/script.js
@@ -0,0 +1,69 @@
+fetch('info.txt')
+  .then(response => response.text())
+  .then(text => {
+    const lines = text.trim().split('\n');
+
+    lines.forEach((line, index) => {
+      const container = document.getElementById(`accordion${index % 3 + 1}`);
+      let [commandName, alternateName, category, syntax, description] = line.split('\t');
+
+      let title = `<span class="command">${commandName}</span>`;
+      if (alternateName != '') {
+        title += `&nbsp;/&nbsp;<span class="command">${alternateName}</span>`;
+      }
+      if (category != '') {
+        title += `<span class="badge ${category.replace(" ", "")}">${category}</span>`;
+      }
+
+      description = description.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+      let descarray = description.split("`");
+      let desc = "";
+      for (let i = 0; i < descarray.length - 1; i++) {
+        desc += descarray[i];
+        if (i % 2 == 0) {
+          desc += `<span class="command">`;
+        } else {
+          desc += `</span>`;
+        }
+      }
+      if (descarray.length > 0) {
+        desc += descarray[descarray.length - 1];
+      }
+
+      if (syntax != "") {
+        desc = `<b>Syntax: </b> <span class="command">${syntax.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</span> <br>${desc}`;
+      }
+
+      const safeId = `collapse${index}`;
+      const accordionItem = `
+        <div class="accordion-item" id="${commandName}">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${safeId}" aria-expanded="false" aria-controls="${safeId}">
+              ${title}
+            </button>
+          </h2>
+          <div id="${safeId}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              ${desc}
+            </div>
+          </div>
+        </div>
+      `;
+      container.insertAdjacentHTML('beforeend', accordionItem);
+    });
+
+    const hash = window.location.hash;
+    if (hash !== "") {
+      const target = document.querySelector(hash);
+      const button = target?.querySelector('button[data-bs-toggle="collapse"]');
+
+      if (target && button) {
+        button.click();
+
+        setTimeout(() => {
+          target.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 200); 
+      }
+    }
+  });
